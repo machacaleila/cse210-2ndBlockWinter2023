2c9da42836e9dfb38ab21e847209f847136a567b
machacaleila <lmachaca@byu.edu>
2023-02-26 02:12:19 -0300

    I added W01 changes

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        int a = -1;
+        while ( a != 0 ){
+            Console.Write("Enter a number: ");
+            string userResponse= Console.ReadLine();
+            a = int.Parse(userResponse);
+            
+            if (a != 0 ){
+                numbers.Add(a);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers){
+            sum +=number;
+        }
+        
+        Console.WriteLine($"The sum is: {sum}");
+        //Average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        //max
+        int max = numbers[0];
+        foreach (int number in numbers){
+              if (number > max){
+                max = number;
+              }
+        }
+        Console.WriteLine($"The max is: {max}");
+
+
     }
 }
