9ee9f34ffa16f297d179dcb39cf125ed7b6f4746
machacaleila <lmachaca@byu.edu>
2023-02-26 01:40:47 -0300

    I added the W01 changes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int numb = randomGenerator.Next(1,101);
+        int guess = -1;
+
+        //Console.Write("Please, guess a number: ");
+        //string chooseNumber = Console.ReadLine();
+        //int guess = int.Parse(chooseNumber);
+        
+        while (guess != numb){
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+             if ( guess < numb ) {
+ 
+             Console.WriteLine("Higher");            
+            }
+            else if (guess > numb) {
+            Console.WriteLine("Lower");
+            }
+            else {
+            Console.WriteLine("You guessed it");
+            }
+        }
+
+       
+
     }
 }
